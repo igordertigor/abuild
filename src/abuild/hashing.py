c8efceea993065e7ae6d4a122e6ac717cd8fd5c2
Ingo Fruend <ingo@oudyo.com>
2024-08-06 14:24:38 +0200

    feat: GlobSkipper implemented

--- src/abuild/hashing.py
+++ src/abuild/hashing.py
@@ -5,15 +5,28 @@ from hashlib import md5
 
 from .ignore import FileSkipperInterface, NeverSkip
 
-def hash_directory(path: Path, skipper: FileSkipperInterface = NeverSkip()) -> str:
+
+def hash_directory(
+    path: Path, skipper: FileSkipperInterface = NeverSkip()
+) -> str:
 
     h = md5()
-    for filepath in sorted(filter(skipper.skip, all_files(path))):
+    for filepath in sorted(all_files(path, skipper)):
         h.update(filepath.read_bytes())
     return h.hexdigest()
 
 
-def all_files(root: Path) -> Iterable[Path]:
+def all_files(root: Path, skipper: FileSkipperInterface) -> Iterable[Path]:
+    skippers = {}
     for dirname, _, filenames in os.walk(str(root)):
+        ignore_file = Path(dirname) / '.abuildignore'
+        if ignore_file.exists():
+            parent_dir = str(Path(dirname).parent)
+            skippers.setdefault(
+                dirname,
+                skippers.get(parent_dir, skipper).update(ignore_file),
+            )
+
         for filename in filenames:
-            return Path(os.path.join(dirname, filename))
+            if not skippers[dirname].skip(Path(filename)):
+                yield Path(os.path.join(dirname, filename))
