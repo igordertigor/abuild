f7a2fc37fee32ed308523d3af894426c2ba170b2
Ingo Fruend <ingo@oudyo.com>
2024-08-08 09:25:54 +0200

    refactor: file collection using recursive function

--- src/abuild/hashing.py
+++ src/abuild/hashing.py
@@ -1,35 +1,44 @@
-from typing import Iterable
-import os
+from typing import Iterable, cast
+import fnmatch
 from pathlib import Path
 from hashlib import md5
 
-from .ignore import FileSkipperInterface, NeverSkip
 
-
-def hash_directory(
-    path: Path, skipper: FileSkipperInterface = NeverSkip()
-) -> str:
+def hash_directory(path: Path) -> str:
 
     h = md5()
-    for filepath in sorted(all_files(path, skipper)):
+    for filepath in sorted(all_files(path, [])):
         h.update(filepath.read_bytes())
     return h.hexdigest()
 
 
-def all_files(root: Path, skipper: FileSkipperInterface) -> Iterable[Path]:
-    skippers: dict[str, FileSkipperInterface] = {}
-    for dirname, _, filenames in os.walk(str(root)):
-        ignore_file = Path(dirname) / '.abuildignore'
-        if ignore_file.exists():
-            parent_dir = str(Path(dirname).parent)
-            skippers.setdefault(
-                dirname,
-                skippers.get(parent_dir, skipper).update(ignore_file),
-            )
-        else:
-            parent_dir = str(Path(dirname).parent)
-            skippers.setdefault(dirname, skippers.get(parent_dir, skipper))
+def all_files(
+    root: Path, patterns: list[str] = cast(list[str], tuple)
+) -> Iterable[Path]:
+    ignore_file = root / '.abuildignore'
+    if ignore_file.exists():
+        patterns = list(
+            set(patterns + ignore_file.read_text().splitlines(keepends=False))
+        )
+    else:
+        patterns = list(set(patterns))
 
-        for filename in filenames:
-            if not skippers[dirname].skip(Path(filename)):
-                yield Path(os.path.join(dirname, filename))
+    for entry in root.glob('*'):
+        if entry == ignore_file:
+            continue
+        elif matches(entry.name, [p.lstrip('/') for p in patterns]) or matches(
+            entry, [f'*{p}' for p in patterns]
+        ):
+            continue
+
+        if entry.is_dir():
+            yield from all_files(entry, [p for p in patterns if p[0] != '/'])
+        else:
+            yield entry
+
+
+def matches(entry: Path | str, patterns: list[str]) -> bool:
+    for pattern in patterns:
+        if fnmatch.fnmatch(str(entry), pattern):
+            return True
+    return False
--- tests/unit/test_ignore.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from abuild.ignore import GlobSkipper
-
-
-def describe_glob_skipper():
-    def test_should_skip_based_on_glob():
-        assert GlobSkipper(['test*']).skip(Path('testfile'))
